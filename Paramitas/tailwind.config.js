22db89d03c1989ac503a03aced44404e62fa88b9
macaequipo <maca.equipo@gmail.com>
2024-12-12 14:28:52 -0300

    Agrego tailwind

---
+++ Paramitas/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'beige-ppal': '#BF8034',
+        'beige-secundario': '#ECBF6C',
+        'manteca': '#F5F5EB',
+        'marron-claro':'#EFE7DA',
+        'marron-medio': '#C2B7A5',
+        'marron': '#B3907A',
+        'marron-oscuro': '#84604E',
+      }
+    },
+  },
+  plugins: [],
+}
+
